be83fb4221a612f5482e817fff7c411c159a8f4a
Lu SpaceAI <irsyadtawakal@gmail.com>
2026-06-04 15:02:55 +0700

    refactor: hapus redundant require middleware/auth.php dari api/me.php

--- api/me.php
+++ api/me.php
@@ -2,7 +2,6 @@
 declare(strict_types=1);
 
 require_once __DIR__ . '/../src/bootstrap.php';
-require_once ROOT_PATH . '/src/middleware/auth.php';
 
 if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
     jsonError('Method tidak diizinkan', 405);
